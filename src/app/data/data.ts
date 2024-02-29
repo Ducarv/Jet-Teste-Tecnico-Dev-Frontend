1bbeab8ba5ef33ad299ef27cf5408a82e3aeb98c
ducarv <educarvalho009@gmail.com>
2024-02-28 20:32:06 -0400

    feat: data example for use

---
+++ src/app/data/data.ts
@@ -0,0 +1,8 @@
+export const data = [
+    { id: 1, name: "Carro 1", photo: "/image.jpg", nextReservation: "01/10", status: "available", rating: 4 },
+    { id: 2, name: "Carro 2", photo: "/image.jpg", nextReservation: "01/10", status: "unavailable", rating: 5 },
+    { id: 3, name: "Carro 3", photo: "/image.jpg", nextReservation: "01/10", status: "available", rating: 5 },
+    { id: 4, name: "Carro 4", photo: "/image.jpg", nextReservation: "01/10", status: "unavailable", rating: 3 },
+    { id: 5, name: "Carro 5", photo: "/image.jpg", nextReservation: "01/10", status: "available", rating: 1 },
+    { id: 6, name: "Carro 6", photo: "/image.jpg", nextReservation: "01/10", status: "unavailable", rating: 2 },
+];
