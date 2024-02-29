bf7d6030acb2fa7112f92375cc5c0aee4dbddc33
ducarv <educarvalho009@gmail.com>
2024-02-28 20:31:06 -0400

    feat: ActionsCell component

---
+++ src/app/components/ActionsCell.tsx
@@ -0,0 +1,9 @@
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+export const ActionsCell = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <MoreVertIcon />
+    </div>
+  );
+}
