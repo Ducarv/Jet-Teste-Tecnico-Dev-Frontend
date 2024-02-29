a51307cf4a259b2079127a30d66718e9a72f31f6
ducarv <educarvalho009@gmail.com>
2024-02-28 20:31:39 -0400

    feat: StatusCell component

---
+++ src/app/components/StatusCell.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Check, Close } from "@mui/icons-material";
+
+interface StatusCellProps {
+  status: string;
+}
+
+export const StatusCell: React.FC<StatusCellProps> = ({ status }) => {
+    const bgColorClass = status.toLowerCase() === "available" ? "bg-green-100" : "bg-red-100";
+    const textColorClass = status.toLowerCase() === "available" ? "text-green-800" : "text-red-800";
+  
+    return (
+      <div className={`px-2 py-1 rounded-full ${bgColorClass} ${textColorClass} text-center text-sm inline-block min-w-min`}>
+        {status}
+      </div>
+    );
+};
