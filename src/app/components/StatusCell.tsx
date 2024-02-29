a16f10a1fad05e43de0121fe5bf4892f21d6ffbe
ducarv <educarvalho009@gmail.com>
2024-02-29 10:00:19 -0400

    refact: use Enum for status

--- src/app/components/StatusCell.tsx
+++ src/app/components/StatusCell.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-
-interface StatusCellProps {
-  status: string;
-}
+import { Status, StatusCellProps } from "../types/index"
 
 export const StatusCell: React.FC<StatusCellProps> = ({ status }) => {
-    const bgColorClass = status.toLowerCase() === "available" ? "bg-green-100" : "bg-red-100";
-    const textColorClass = status.toLowerCase() === "available" ? "text-green-800" : "text-red-800";
+    const bgColorClass = status.toLowerCase() === Status.Available ? "bg-green-100" : "bg-red-100";
+    const textColorClass = status.toLowerCase() === Status.Available ? "text-green-800" : "text-red-800";
   
     return (
       <div className={`px-2 py-1 rounded-full ${bgColorClass} ${textColorClass} text-center text-sm inline-block min-w-min`}>
